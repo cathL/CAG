1905512a1e2c8207c6518926f4b56af5c97b2106
cathL <c.lebeau@ac-nancy-metz.fr>
2019-04-02 13:59:42 +0200

    essai

--- src/Repository/ContratRepository.php
+++ src/Repository/ContratRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Contrat;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Psr\Log\NullLogger;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
@@ -17,5 +18,12 @@ class ContratRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Contrat::class);
+    }
+
+    public function nbContrats($negociant) {
+        //$lesContrats = $this->findByNegociant($negociant);
+
+        //return count($lesContrats);
+        return null;
     }
 }
